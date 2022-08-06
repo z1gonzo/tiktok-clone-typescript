377197cc81315b7bcc840be9d0bae95ce1deeeb5
z1gonzo <goonsky@gmail.com>
2022-08-06 21:35:07 +0200

    main video view

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -17,8 +17,10 @@ const Sidebar = () => {
 
   const userProfile = false
 
-  const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded'
-  const activeLink = ''
+  const activeLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded';
+
+  const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold rounded';
+
 
 
   return (
@@ -29,19 +31,20 @@ const Sidebar = () => {
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
       {showSidebar && (
-        <div className="xl:w-400 w-200 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+        <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
           <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
             <Link href="/">
               <div className={normalLink}>
                 <p className="text-2xl">
                   <AiFillHome />
                 </p>
-                <span className='text-xl hidden xl:block'>
+                <span className='capitalize text-xl hidden xl:block'>
                   For You
                 </span>
               </div>
             </Link>
           </div>
+
           {!userProfile && (
             <div className='px-2 py-4 hidden xl:block'>
               <p className='text-gray-400'>Log in to like and comment on videos</p>
--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+  projectId: 'uustn7sh',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
