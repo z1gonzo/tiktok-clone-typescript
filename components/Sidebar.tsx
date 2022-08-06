8a7156bc95ac1a6fa375a47870bf7f67f1989541
z1gonzo <goonsky@gmail.com>
2022-08-07 01:49:39 +0200

    video at details and mute

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 
 import Link from 'next/link'
-// import GoogleLogin from 'react-google-login'
+
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai'
 import { ImCancelCircle } from 'react-icons/im'
 
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/router';
+import { GoVerified } from 'react-icons/go';
+import Image from 'next/image';
+import Link from 'next/link';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 
-const Detail = () => {
+// import Comments from '../../components/Comments';
+import { BASE_URL } from '../../utils';
+// import LikeButton from '../../components/LikeButton';
+import useAuthStore from '../../store/authStore';
+import { Video } from '../../types';
+import axios from 'axios';
+
+interface IProps {
+  postDetails: Video;
+}
+
+const Detail = ({ postDetails }: IProps) => {
+  const [post, setPost] = useState(postDetails)
+  const [playing, setPlaying] = useState(false)
+  const [isVideoMuted, setIsVideoMuted] = useState(false)
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const router = useRouter()
+
+  const onVideoClick = () => {
+    if (playing) {
+      videoRef?.current?.pause()
+      setPlaying(false)
+    } else {
+      videoRef?.current?.play()
+      setPlaying(true)
+    }
+  }
+
+  useEffect(() => {
+    if (post && videoRef?.current) {
+      videoRef.current.muted = isVideoMuted
+    }
+  }, [post, isVideoMuted])
+
+
+
+  if (!post) return null;
+
+
   return (
-    <div>[id]</div>
+    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+      <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
+        <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
+          <p
+            onClick={() => router.back()}
+            className='cursor-pointer'>
+            <MdOutlineCancel className='text-white text-[35px]' />
+          </p>
+        </div>
+        <div className='relative'>
+          <div className='lg:h-[100vh] h-[60vh]'>
+            <video
+              ref={videoRef}
+              loop
+              onClick={onVideoClick}
+              src={post.video.asset.url}
+              className="h-full cursor-pointer"
+            >
+            </video>
+          </div>
+          <div className='absolute top-[45%] left-[45%] cursor-pointer'>
+            {!playing && (
+              <button onClick={onVideoClick}>
+                <BsFillPlayFill className='text-white text-6xl lg:text-8xl' />
+              </button>
+            )}
+          </div>
+        </div>
+        <div className='absolute bottom-5 lg:bottom-10 right-5 lg:right-10 cursor-pointer'>
+          {isVideoMuted ? (
+            <button onClick={() => setIsVideoMuted(false)}>
+              <HiVolumeOff className='text-white text-2xl lg:text-4xl' />
+            </button>
+          ) : (
+            <button onClick={() => setIsVideoMuted(true)}>
+              <HiVolumeUp className='text-white text-2xl lg:text-4xl' />
+            </button>
+          )}
+        </div>
+
+      </div>
+    </div>
   )
+}
+
+export const getServerSideProps = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const { data } = await axios.get(`${BASE_URL}/api/post/${id}`);
+
+  return {
+    props: { postDetails: data },
+  }
 }
 
 export default Detail
